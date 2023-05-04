bf0fe05e069336d135175fac967935b114db6452
elisabeth <elisabethanne81@gmail.com>
2023-05-04 13:56:43 -0400

    calling on objects rather than classes (thanks JJ)

--- weapon.py
+++ weapon.py
@@ -3,3 +3,7 @@ class Weapon:
     def __init__(self):
         self.name = 'Lightsaber'
         self.attack_power = 50
+
+    def swing(self, Dinosaur):
+        Dinosaur.health -= self.attack_power
+
